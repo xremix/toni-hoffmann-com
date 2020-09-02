a912114b3ba01b7a04cd0ee6b5e559c5a733eedd
Toni Hoffmann <xremix@users.noreply.github.com>
2020-09-02 19:50:06 +0200

    preparing code for photo modal

--- src/app/components/photography/album/album.component.ts
+++ src/app/components/photography/album/album.component.ts
@@ -13,17 +13,34 @@ export class AlbumComponent implements OnInit {
   public subTitle: string;
   public images: Array<any>;
   private sub: any;
+  private modalPhoto: any;
 
   constructor(private route: ActivatedRoute, private photoService: PhotoService) { }
 
   ngOnInit(): void {
     this.sub = this.route.params.subscribe(params => {
-      var albumParameter = params['album']; // (+) converts string 'id' to a number
+      var albumParameter = params['album'];
       var album = this.photoService.getAlbum(albumParameter);
       this.images = album.photos;
       this.title = album.title;
       this.subTitle = album.subTitle;
       // In a real app: dispatch action to load the details here.
+      
+      // Show photo modal
+      var photoIdParameter = params['photoId'];
+      if(photoIdParameter){
+        var photoFromParameter = null;
+        // TODO search the correct photo from the this.images array
+        showPhotoModal(photoFromParamter);
+      }
+      
     });
+  }
+  
+  showPhotoModal(photo: any){
+    // Show the Modal
+  }
+  
+  hidePhotoModal(){
   }
 }
