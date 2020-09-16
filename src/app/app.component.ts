efd29c3a8a0efbcc3c8800522c913bb6b00aae52
xremix <tonidhoffmann@gmail.com>
2020-09-16 23:22:37 +0200

    close navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { AnalyticsService } from 'src/app/services/analytics.service';
+import { NavigationComponent } from 'src/app/components/shared/navigation/navigation.component'
 
 @Component({
   selector: 'app-root',
@@ -8,11 +9,13 @@ import { AnalyticsService } from 'src/app/services/analytics.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'toni-hoffmann-com';
+  @ViewChild('navigation') navigation: NavigationComponent;
+
   constructor(public router: Router, public analyaticsService: AnalyticsService){
       this.router.events.subscribe(event => {
          if(event instanceof NavigationEnd){
            this.analyaticsService.init(event);
+           this.navigation.isNavbarCollapsed = true;
           }
        }
     )}
