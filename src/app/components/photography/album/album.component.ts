7e63427795242cbe4270545338e775a783dec928
Toni Hoffmann <xremix@users.noreply.github.com>
2020-09-18 13:37:58 +0000

    make album pages independent of array index

--- src/app/components/photography/album/album.component.ts
+++ src/app/components/photography/album/album.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { PhotoService } from 'src/app/services/photo.service';
 import { SeoService } from 'src/app/services/seo.service';
 import { PhotoModalComponent } from './photo-modal/photo-modal.component';
@@ -21,14 +21,14 @@ export class AlbumComponent implements OnInit {
   public callToAction = window.screen.height * 2;
 
   private pageSize: number = 21;
-  private page: number = 0;
+  private page: number = 1;
 
 
   public pages: Array<number> = [];
 
   @ViewChild('photoModal') photoModal: PhotoModalComponent;
 
-  constructor(private seoService: SeoService, private photoService: PhotoService, private route: ActivatedRoute) {
+  constructor(public router: Router, private seoService: SeoService, private photoService: PhotoService, private route: ActivatedRoute) {
     route.params.subscribe(params => {
     this.createGallery(params);
   });
@@ -43,14 +43,21 @@ export class AlbumComponent implements OnInit {
   createGallery(params: any){
     // TODO Make pages based on 1 instead of 0
       var albumParameter = params['album'];
-      if(params['page']){
-        this.page = +params['page'];
-      }
       this.album = this.photoService.getAlbumMetaData(albumParameter);
       this.seoService.updatePageMetaData(
         `${this.album.title} Photography by Toni Hoffmann`,
         this.album.subTitle
       );
+
+      if(params['page']){
+        this.page = +params['page'];
+      }else{
+        var url = `/photography/${this.album.id}/1`;
+        
+        this.router.navigate([url], { relativeTo: this.route });
+
+      }
+
       this.photoService.getPhotosFromAlbum(albumParameter).subscribe(data =>{
 
         data = data.map(i =>{
@@ -63,11 +70,11 @@ export class AlbumComponent implements OnInit {
 
         this.pages = UtilitiesService.fillArray(pages.length);
 
-        if(this.page + 1 > this.pages.length){
-          window.location.href = '/404';
+        if(this.page > this.pages.length){
+          this.router.navigate(['/404'], { relativeTo: this.route });
         }
 
-        this.album.photos = pages[this.page];
+        this.album.photos = pages[this.page - 1];
 
         this.images = this.album.photos;
         console.log("create gallery");
